13e7849c10201e3ea18147a8e89f0973e78ed8d6
LAPTOP-QCIUTNP8\a0919 <a0919025694@gmail.com>
2023-12-18 03:50:31 +0800

    fix

--- index-new.js
+++ index-new.js
@@ -1,27 +1,35 @@
+const images = document.querySelectorAll('.index-content-item-pic');
+const copyButton = document.getElementById('copyButton');
+const copyText = document.getElementById('copyText');
 
-
-
-  const images = document.querySelectorAll('.index-content-item-pic');
-
-  images.forEach((image) => {
-    image.addEventListener('mouseover', () => {
-      const backgroundImage = image.style.backgroundImage;
-      document.body.style.backgroundImage = backgroundImage;
-    });
-
-    image.addEventListener('click', () => {
-      const backgroundImage = image.style.backgroundImage;
-      document.body.style.backgroundImage = backgroundImage;
-    });
+images.forEach((image) => {
+  image.addEventListener('mouseover', () => {
+    const backgroundImage = image.style.backgroundImage;
+    setBodyBackgroundImage(backgroundImage);
   });
 
+  image.addEventListener('click', () => {
+    const backgroundImage = image.style.backgroundImage;
+    setBodyBackgroundImage(backgroundImage);
+  });
+});
 
+function setBodyBackgroundImage(backgroundImage) {
+  document.body.style.backgroundImage = backgroundImage;
+  document.body.style.backgroundSize = '100%';  // 使用 'contain' 保持圖片比例並填滿視窗
+  document.body.style.backgroundRepeat = 'repeat';  // 圖片重複
+}
 
-  const copyButton = document.getElementById('copyButton');
-  const copyText = document.getElementById('copyText');
+// 檢查視窗寬度
+function isDesktop() {
+  return window.innerWidth >= 768;
+}
 
+// 添加事件監聽器
+if (isDesktop()) {
   copyButton.addEventListener('click', () => {
     copyText.select();
     document.execCommand('copy');
-    ;
   });
+}
+
